6d98d93d13311fd5a8d2ed78409c120707796c96
doumplingg <dvntineenr@gmail.com>
2024-11-28 12:14:34 +0700

    add new code

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
 public class Main{
     public static void main(string[] args){
      System.out.println("Hello world"); 
+    
+    String [] books = ("Java Programming", "Database Design", "with Development");
+    System.out.println("Available Books: ");
+    for (String book : books) {
+        System.out.println("- " + book);
+    }
     }
 }
